268d5b9c92d97324ee72a61fd7687b145b15899d
Antoine Chopard <antoinechopard@MacBook-Pro-de-Antoine.local>
2024-09-30 10:08:44 +0200

    ex 4

---
+++ ex_04.js
@@ -0,0 +1,8 @@
+// Code pour afficher des messages sur la page
+document.body.innerHTML = `
+    <h1>Bienvenue dans la piscine JavaScript !</h1>
+    <p>Amusez-vous et n'oubliez pas d'apporter une serviette.</p>
+`;
+
+// Code pour afficher un message dans la console
+console.log("Site web lancé !");
